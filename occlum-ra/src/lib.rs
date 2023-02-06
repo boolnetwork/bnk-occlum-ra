3d098131b6d9e73ae4ed777036e061132d8a67ac
prz23 <panruizheng@zju.edu.cn>
2023-02-06 16:44:00 +0800

    add code comments

--- occlum-ra/src/lib.rs
+++ occlum-ra/src/lib.rs
@@ -10,11 +10,13 @@ pub use verify::{verify, verify_only_report};
 use attestation::{EnclaveFields, DcapAttestation};
 use std::time::{SystemTime, UNIX_EPOCH};
 
+/// return (key_der,cert_der)
 pub fn generate_cert_key() -> Result<(Vec<u8>,Vec<u8>),String>{
     println!("start generate_cert_key");
     generate_cert("".to_string())
 }
 
+/// verify cert_der
 pub fn verify_cert(cert:&[u8]) -> Result<EnclaveFields, String>{
     let now = SystemTime::now()
         .duration_since(UNIX_EPOCH)
@@ -23,6 +25,7 @@ pub fn verify_cert(cert:&[u8]) -> Result<EnclaveFields, String>{
     verify(cert,now)
 }
 
+/// create dcap report with additional info
 pub fn create_dcap_report(additional_info:Vec<u8>) -> Result<Vec<u8>, String>{
     let report = match DcapAttestation::create_report(&additional_info) {
         Ok(r) => r,
@@ -31,6 +34,7 @@ pub fn create_dcap_report(additional_info:Vec<u8>) -> Result<Vec<u8>, String>{
     Ok(report.into_payload())
 }
 
+/// verify dcap report with additional info and return (mr_enclave.m,report_data.d)
 pub fn verify_dcap_report(report: Vec<u8>)  -> Result<(Vec<u8>, Vec<u8>), String>{
     let now = SystemTime::now()
         .duration_since(UNIX_EPOCH)
