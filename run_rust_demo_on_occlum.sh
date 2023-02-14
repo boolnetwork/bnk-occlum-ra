1b4b33255f4757a359ca6bdbe0303bd4ab317399
prz23 <panruizheng@zju.edu.cn>
2023-02-14 14:05:41 +0800

    update

--- run_rust_demo_on_occlum.sh
+++ run_rust_demo_on_occlum.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# build gmp-musl
-chmod +x ./gmp_musl.sh
-./gmp_musl.sh
-
 # compile rust_app
 pushd rust_app
 occlum-cargo build
@@ -15,8 +11,6 @@ rm -rf occlum_instance && mkdir occlum_instance && cd occlum_instance
 
 occlum init && rm -rf image
 cp -r ../rust_app/Occlum.json ../occlum_instance/
-cp -r ../rust_app/key_config.toml ../occlum_instance/
-cp ../../sgx_default_qcnl.conf /etc
 copy_bom -f ../rust-demo.yaml --root image --include-dir /opt/occlum/etc/template
 
 occlum build
