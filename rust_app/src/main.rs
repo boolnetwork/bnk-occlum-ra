041ff5964ab77097151ecf02b75446f62b2b95dc
prz23 <panruizheng@zju.edu.cn>
2023-10-07 15:26:03 +0800

    add pailliar

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -7,6 +7,8 @@ use occlum_ra::attestation::{AttestationReport, AttestationStyle, IasAttestation
 
 #[cfg(not(target_env = "msvc"))]
 use tikv_jemallocator::Jemalloc;
+use zk_paillier::zkproofs::BlumModulesProof;
+
 #[cfg(not(target_env = "msvc"))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
@@ -15,35 +17,12 @@ fn main() {
     env_logger::init();
 
     println!("start");
-    let cert_der = match generate_cert_key() {
-        Err(e) => panic!("error: {:?}", e),
-        Ok((a, b)) => b,
-    };
-    let res = verify_cert(&cert_der);
-    println!("verify_cert result {:?}", res);
+    let (ek, dk) = Paillier::keypair_safe_primes().keys();
+    println!("======start======");
+    assert_eq!(&dk.p % 4, BigInt::from(3));
+    assert_eq!(&dk.q % 4, BigInt::from(3));
+    let proof = BlumModulesProof::prove(ek.n.clone(), dk.p.clone(), dk.q.clone()).unwrap();
+    assert!(proof.verify(ek.n));
 
-    println!("GMP rust bindings test");
-    let two: Mpz = From::<i64>::from(2);
-    let eight: Mpz = From::<i64>::from(8);
-    let minuseight: Mpz = From::<i64>::from(-8);
-    let three: Mpz = From::<i64>::from(3);
-    let minusthree: Mpz = From::<i64>::from(-3);
-    assert_eq!(eight.div_floor(&three), two);
-    assert_eq!(eight.div_floor(&minusthree), minusthree);
-    assert_eq!(minuseight.div_floor(&three), minusthree);
-    assert_eq!(minuseight.div_floor(&minusthree), two);
-    println!("test_div_floor pass");
-    let one: Mpz = From::<i64>::from(1);
-    let minusone: Mpz = From::<i64>::from(-1);
-    let two: Mpz = From::<i64>::from(2);
-    let minustwo: Mpz = From::<i64>::from(-2);
-    let three: Mpz = From::<i64>::from(3);
-    let minusthree: Mpz = From::<i64>::from(-3);
-    let eight: Mpz = From::<i64>::from(8);
-    let minuseight: Mpz = From::<i64>::from(-8);
-    assert_eq!(eight.mod_floor(&three), two);
-    assert_eq!(eight.mod_floor(&minusthree), minusone);
-    assert_eq!(minuseight.mod_floor(&three), one);
-    assert_eq!(minuseight.mod_floor(&minusthree), minustwo);
     println!("test_mod_floor pass");
 }
