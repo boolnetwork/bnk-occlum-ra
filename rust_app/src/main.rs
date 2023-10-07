2049df04a7efe9d4f42823df0be8f572cc57be2f
prz23 <panruizheng@zju.edu.cn>
2023-10-07 16:09:26 +0800

    mark

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -1,9 +1,9 @@
-use occlum_ra;
+// use occlum_ra;
 
-use occlum_ra::{generate_cert_key, generate_epid, get_fingerprint, get_fingerprint_epid, verify_cert};
+// use occlum_ra::{generate_cert_key, generate_epid, get_fingerprint, get_fingerprint_epid, verify_cert};
 use std::time::{SystemTime, UNIX_EPOCH};
 use gmp::mpz::Mpz;
-use occlum_ra::attestation::{AttestationReport, AttestationStyle, IasAttestation};
+// use occlum_ra::attestation::{AttestationReport, AttestationStyle, IasAttestation};
 
 #[cfg(not(target_env = "msvc"))]
 use tikv_jemallocator::Jemalloc;
